06a7c91bc5aceebce11a6a9e892a5b741defffdb
John Green <jpgreen@Johns-iMac.local>
2018-07-17 01:38:56 -0400

    Lesson Completed

--- QuestionBot/QuestionAnswerer.swift
+++ QuestionBot/QuestionAnswerer.swift
@@ -1,6 +1,33 @@
 struct MyQuestionAnswerer {
     func responseTo(question: String) -> String {
-        // TODO: Write a response 
-        return "?"
+        
+        let lowerCasedQuestion = question.lowercased()
+        
+        if lowerCasedQuestion == "hello there" {
+            return "Why, hello there!"
+        }
+            
+        else if lowerCasedQuestion == "where should i go on holiday?" {
+            return "To the North!"
+        }
+            
+        else if lowerCasedQuestion == "where can i find the north pole?" {
+            return "To the North!"
+        }
+            
+        else if lowerCasedQuestion == "where are the cookies?" {
+            return "In the cookie jar!"
+        }
+            
+        else if lowerCasedQuestion == "can i have a cookie?" {
+            return "Yes, you may have TWO cookies!!"
+        }
+            
+        else if question == "CAN I HAVE A COOKIE?!?" {
+            return "Not if you can't ask without shouting!"
+        }
+            
+        else  { return "Ask another question that I can answer" }
+        
     }
 }
